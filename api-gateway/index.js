d42780d86b20a6c1dd8d1a3c93382f93517b24e1
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2025-01-07 02:08:15 +0600

    package update for api-gateway

---
+++ api-gateway/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const routes = require('./routes');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`API Gateway listening at http://localhost:${port}`);
+});
